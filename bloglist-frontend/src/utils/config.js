e8d83c85846e7b880b2f2afb9b7bffb0df6efaee
nigosH <32242577+nigoshh@users.noreply.github.com>
2018-07-19 19:47:25 +0200

    fixing backend url for heroku 7th try

--- bloglist-frontend/src/utils/config.js
+++ bloglist-frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 let backendUrl = ''
 
 if (process.env.NODE_ENV === 'production')
-  backendUrl = process.env.BACKEND_URL
+  backendUrl = process.env.REACT_APP_BACKEND_URL
 
 module.exports = { backendUrl }
